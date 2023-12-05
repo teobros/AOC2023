cb7106c8f9a81f70e19d3eadf283cc9232a26bcb
Matteo Broggi <broggi@irz.uni-hannover.de>
2023-12-05 15:37:46 +0100

    Solve day 5 part 1
    Completely new function that does not expand range solved the extreme
    memory usage problem

--- day5/day5.jl
+++ day5/day5.jl
@@ -24,68 +24,42 @@ function get_map(lines)
     return parsed_map, idx_line
 end
 
-function expand_map(map_orig)
-    source = reshape([],0,1)
-    destination_temp = reshape([],0,1)
-    for i in 1:size(map_orig,1)
-        source = vcat(source,collect(range(map_orig[i,2]+1,map_orig[i,2]+map_orig[i,3]))) 
-        # +1 because the index are based 0 in the question
-        destination_temp = vcat(destination_temp,collect(range(map_orig[i,1]+1,map_orig[i,1]+map_orig[i,3])))
-    end
-    destination = collect(range(1,maximum(source)))
-    destination[source] = destination_temp
-    return destination
-end
-
-function location(seed,maps_sequence)
-    destination = source2dest(seed,maps_sequence[1])
-    for imap = 2:length(maps_sequence)
-        destination = source2dest(destination,maps_sequence[imap])
-    end
-    return destination
-end
 function source2dest(source,map)
-    if source > maximum(map)
-        dest = source
-    else
-        dest = map[source]
+    dest=source
+    for map_int in eachrow(map)
+        if source in map_int[2]:map_int[2]+map_int[3]-1
+            dest = map_int[1]+(source - map_int[2])
+        end
     end
     return dest
 end
 
 
 seed_to_soil, nlines = get_map(lines)
-mapping = expand_map(seed_to_soil)
-soils = map(x->source2dest(x,mapping), seeds)
+soils = map(x->source2dest(x,seed_to_soil), seeds)
 deleteat!(lines, 1:nlines+1)
 
 soil_to_fertilizer, nlines = get_map(lines)
-mapping = expand_map(soil_to_fertilizer)
-fertilizers = map(x->source2dest(x,mapping), soils)
+fertilizers = map(x->source2dest(x,soil_to_fertilizer), soils)
 deleteat!(lines, 1:nlines+1)
 
 fertilizer_to_water, nlines = get_map(lines)
-mapping = expand_map(fertilizer_to_water)
-waters = map(x->source2dest(x,mapping), fertilizers)
+waters = map(x->source2dest(x,fertilizer_to_water), fertilizers)
 deleteat!(lines, 1:nlines+1)
 
 water_to_light, nlines = get_map(lines)
-mapping = expand_map(water_to_light)
-lights = map(x->source2dest(x,mapping), waters)
+lights = map(x->source2dest(x,water_to_light), waters)
 deleteat!(lines, 1:nlines+1)
 
 light_to_temperature, nlines = get_map(lines)
-mapping = expand_map(light_to_temperature)
-temperatures = map(x->source2dest(x,mapping), lights)
+temperatures = map(x->source2dest(x,light_to_temperature), lights)
 deleteat!(lines, 1:nlines+1)
 
 temperature_to_humidity, nlines = get_map(lines)
-mapping = expand_map(temperature_to_humidity)
-humidities = map(x->source2dest(x,mapping), temperatures)
+humidities = map(x->source2dest(x,temperature_to_humidity), temperatures)
 deleteat!(lines, 1:nlines+1)
 
 humidity_to_location, nlines = get_map(lines)
-mapping = expand_map(humidity_to_location)
-locations = map(x->source2dest(x,mapping), humidities)
+locations = map(x->source2dest(x,humidity_to_location), humidities)
 
 answer1 = minimum(locations)
