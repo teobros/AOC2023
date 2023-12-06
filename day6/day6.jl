daa3a30321e77671edaf5d9b04fff4c540d30742
Matteo Broggi <broggi@irz.uni-hannover.de>
2023-12-06 11:29:52 +0100

    Solve day 5 part 2 on simple test file

--- day6/day6.jl
+++ day6/day6.jl
@@ -1,4 +1,4 @@
-lines = readlines("input.txt")
+lines = readlines("input_test.txt")
 
 function line2vec(line)
     line = line[findfirst(isequal(':'),line)+2:end]
@@ -19,3 +19,19 @@ racewins = (x,y)->sum(race2distance(x).>y)
 wins = map(racewins, time, distance)
 
 answer1 = prod(wins)
+
+## part 2
+
+function vec2total(vec)
+    total=vec[end]
+
+    oom = x-> floor(Int, log10(x))
+
+    for i in length(vec)-1:-1:1
+        total += vec[i]*10^(oom(total)+1)
+    end
+    return total
+end
+
+time2 = vec2total(time)
+distance2 = vec2total(distance)
